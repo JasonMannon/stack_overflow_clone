fa9d704b5e98d4fefcf881935c58485d4db7889e
Jason Mannon & Emily Zhang <student@epicodus.com>
2014-04-14 09:29:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StackOverflowClone::Application.config.secret_key_base = 'a6a8cbae06ebea1bdd62fed94ab1134c5f167cc13d7c3a1d23d7cec5032a8392e78c794436d3d8a9c25e08c534d13edcdb511815ee6fa133e714375d4aa02c66'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+StackOverflowClone::Application.routes.draw do
+  resources :users
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy' as: 'logout'
+
+end
